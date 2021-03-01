840c5321d417f6110c1839451c63d80f49dfa073
Mehrdad Asalian <lost.mhr69@gmail.com>
2021-03-01 15:12:52 +0330

    finish

---
+++ src/test/java/com/mhr/eureka/EurekaDiscoveryServiecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhr.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
